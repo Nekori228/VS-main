3061f0b87aa4865c41b0d65356a76d58ed13afad
nekori <Nekori>
2022-10-10 10:28:04 +0530

    Correct edit.dart

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -25,31 +25,100 @@ class Edit extends StatelessWidget {
           child: (Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(15, 20, 0, 10),
-                  child: Text(
-                    'Название',
-                    style: TextStyle(color: Colors.black, fontSize: 21),
-                  ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(15, 20, 0, 3),
+                child: Text(
+                  'Название',
+                  style: TextStyle(color: Colors.black, fontSize: 21),
                 ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(15, 20, 0, 10),
-                  child: Text(
-                    'Водитель такси',
-                  style: TextStyle(color: Colors.grey, fontSize: 13),),
-                ),
+              ),
               Center(
                 child: SizedBox(
-                  width: 330,
-                  height: 1,
+                    height: 40,
+                    width: 330,
+                    child: TextField(
+                      decoration: InputDecoration(hintText: "Водитель такси"),
+                      style: TextStyle(color: Colors.black, fontSize: 13),
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(15, 30, 0, 3),
+                child: Text(
+                  'Телефон',
+                  style: TextStyle(color: Colors.black, fontSize: 21),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                    height: 40,
+                    width: 330,
+                    child: TextField(
+                      decoration: InputDecoration(hintText: "8-911-111-22-33"),
+                      style: TextStyle(color: Colors.black, fontSize: 13),
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(15, 30, 0, 3),
+                child: Text(
+                  'Описание',
+                  style: TextStyle(color: Colors.black, fontSize: 21),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                    height: 40,
+                    width: 330,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Водитель Владимир; чётные даты"),
+                      style: TextStyle(color: Colors.black, fontSize: 13),
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: GestureDetector(
+                  onTap: () {
+                    print('Test1');
+                  },
                   child: Container(
                     decoration: BoxDecoration(
-                        border: Border(
-                            bottom:
-                                BorderSide(width: 1.0, color: Colors.black))),
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.white),
+                        color: Colors.blue),
+                    margin: EdgeInsets.all(15),
+                    width: 340,
+                    height: 60,
+                    child: Center(
+                      child: Text('ДОБАВИТЬ',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500)),
+                    ),
                   ),
                 ),
-              )
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('Test2');
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.white),
+                      color: Colors.blue),
+                  margin: EdgeInsets.all(15),
+                  width: 340,
+                  height: 60,
+                  child: Center(
+                    child: Text('УДАЛИТЬ',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500)),
+                  ),
+                ),
+              ),
             ],
           )),
         ));
